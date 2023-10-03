12f7e16fe413460920b2d1cec1e730d305829dcf
gwhitson <112592268+gwhitson@users.noreply.github.com>
2023-10-03 10:00:37 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,5 +17,6 @@ git clone https://github.com/gwhitson/ubuntu-setup
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-cd ubuntu-setup
-mv ./nvim ~/.config/nvim
+mv ~/ubuntu-setup/nvim ~/.config/nvim
+
+rm -rf ~/ubuntu-setup
