9151a0e5206c2a105e7a23bb4aa8d3a5e48e61cc
Gavin Whitson <rockywhitson4@gmail.com>
2023-10-16 10:44:25 -0500

    update nvim config

--- nvim/lua/gavin/remap.lua
+++ nvim/lua/gavin/remap.lua
@@ -1,9 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ef", vim.cmd.Ex)
 
---vim.keymap.set('n', '<C-I>', "gv")
-vim.keymap.set("v","K", ":m '<-2<CR> gv")
 vim.keymap.set("v","J", ":m '>+1<CR> gv")
+vim.keymap.set("v","K", ":m '<-2<CR> gv")
 
 vim.keymap.set("n","J","mzJ`z")
 vim.keymap.set("n","<C-d>","<C-d>zz")
