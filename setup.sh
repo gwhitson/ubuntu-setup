b3e51b09d74a504de5e5ddd0ed6d8cbf66f010b9
gavin <rockywhitson4@gmail.com>
2023-11-14 16:10:51 -0600

    update

--- setup.sh
+++ setup.sh
@@ -13,11 +13,23 @@ if [ $? -eq 1 ] ; then
     sudo snap install nvim --classic
 fi
 
+ls ~/.local/share | grep -q "ubuntu-setup"
+if [ $? -eq 0 ] ; then
+	rm -rf ~/.local/share/ubuntu-setup
+fi
 git clone https://github.com/gwhitson/ubuntu-setup ~/.local/share/ubuntu-setup
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+ls ~/.local/share/nvim/site/pack | grep -q "packer"
+if [ $? -eq 1 ] ; then
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+fi
 
+
+ls ~/.config | grep -q "nvim"
+if [ $? -eq 0 ] ; then
+	rm -rf ~/.config/nvim
+fi
 ln -s ~/.local/share/ubuntu-setup/nvim ~/.config/nvim
 
 sudo rm /bin/editor
@@ -35,13 +47,10 @@ fi
 
 
 ls ~/.ssh | grep -q ".pub"
-if [ $? -eq 1 ]
+if [ $? -eq 1 ] ; then
     ssh-keygen -t rsa -b 2048  -f ~/.ssh/git_key -N ""
 else
     echo "ensure SSH key has been uploaded to github"
 fi
 
 sudo apt upgrade -y
-
-
-rm ~/Downloads/setup.sh
